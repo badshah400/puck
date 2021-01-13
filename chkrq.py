ff8e0de48d8b1b9ca911631666052e89420253cf
Atri Bhattacharya <A.Bhattacharya@uliege.be>
2021-01-13 15:48:33 +0100

    chkrq: Fix main usage.

--- chkrq.py
+++ chkrq.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # vim: set ai et ts=4 sw=4 tw=80 fileencoding=utf-8:
 
+import sys
 import osc.conf
 import osc.core
+from pkglistparse import PrjPkgList
 from lxml import etree
 
 def chkrq(prj,pkg):
@@ -31,5 +33,16 @@ def chkrq(prj,pkg):
 
 if __name__ == '__main__':
     osc.conf.get_config()
-    u, msg = chkrq('science', 'plplot')
-    print('{:s} [{:s}]'.format(u, msg) if msg else 'None')
+    pkgs = []
+    if len(sys.argv) == 1:
+        pkgs = ['science/plplot']
+    else:
+        for a in sys.argv[1:]:
+            pkgs.append(a)
+
+    f = PrjPkgList(pkgs)
+
+    for prj, pkg in f.List():
+        u, msg = chkrq(prj, pkg)
+        if msg:
+            print('{:s} [{:s}]'.format(u, msg))
