f1acecc681545af29043507f94e38606d5e61807
Atri Bhattacharya <A.Bhattacharya@uliege.be>
2021-01-13 16:15:23 +0100

    chkrq: Check if sr target project/package matches.

--- chkrq.py
+++ chkrq.py
@@ -8,28 +8,32 @@ from pkglistparse import PrjPkgList
 from lxml import etree
 
 def chkrq(prj,pkg):
-    apiurl = osc.conf.config['apiurl']
-
-    u   = osc.core.makeurl(apiurl, ['request'],
-                           query = { 'view'    : 'collection', 
-                                     'project' : prj,
-                                     'package' : pkg,
-                                     'limit'   : 1,
-                                     'states'  : 'new,review' })
-    fi = osc.core.http_GET(u)
+    apiurl  = osc.conf.config['apiurl']
+    u       = osc.core.makeurl(apiurl, ['request'],
+                               query = { 'view'    : 'collection',
+                                         'project' : prj,
+                                         'package' : pkg,
+                                         'limit'   : 1,
+                                         'states'  : 'new,review' })
+    fi      = osc.core.http_GET(u)
 
     # Join all lines and read them into an etree object
     collxml = etree.fromstring(b''.join(fi.readlines()))
-
-    nreq = int(collxml.get('matches'))                # Get no. of new/review requests
+    nreq    = int(collxml.get('matches'))             # Get no. of new/review requests
 
     if not nreq:                                      # nreq = 0 => no requests
         return (u'', None)
     else:
-        descr = collxml[0].find('description').text   # Get text in <description> node...
-        descr = descr.splitlines()[0]                 # ... keeping only first line
-
-        return (u'r', descr)
+        target = collxml[0].find('.//target')         # To check if target matches
+        tgtprj = target.get('project')
+        tgtpkg = target.get('package')
+        if (tgtprj == prj) and (tgtpkg == pkg):
+            descr = collxml[0].find('description').text
+                                                      # Get text in <description> node...
+            descr = descr.splitlines()[0]             # ... keeping only first line
+            return (u'r', descr)
+        else:
+            return (u'', None)
 
 if __name__ == '__main__':
     osc.conf.get_config()
