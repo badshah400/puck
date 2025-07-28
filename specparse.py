a48ddd6d9b26a7deba32f7ec1a5e42bb85f215a1
Atri Bhattacharya <A.Bhattacharya@uliege.be>
2025-07-28 11:30:06 +0530

    feat: For multibuild spec, grep RPM tags directly.
    Instead of simply bailing out when encountering multibuild packages,
    because `rpmspec` does not work with the associated OBS specific macros,
    try grep-ing for Source, URL, and Version RPM tags directly instead.

--- specparse.py
+++ specparse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # vim: set ai et ts=4 sw=4 tw=100:
+# mypy: disable-error-code=import-untyped
 
 from os import path, mkdir
 from subprocess import run, CalledProcessError
@@ -17,8 +18,12 @@ UNDEFINED_MACROS = ['openmpi_requires', 'sysusers_requires']
 
 
 class SpecTags:
-    re_src0 = re.compile('Source0?:.*')
-    apiurl  = osc.conf.config['apiurl']
+    # Useful pre-compiled regexes
+    RE_SRC0 = re.compile('^Source0?:.*', flags=re.MULTILINE)
+    RE_URL  = re.compile('^(URL|Url):.*', flags=re.MULTILINE)
+    RE_VER  = re.compile('^Version:.*', flags=re.MULTILINE)
+
+    API_URL = osc.conf.config['apiurl']
 
     def __init__(self, prj, pkg):
         self.name    = ''
@@ -27,20 +32,21 @@ class SpecTags:
         self.src_url = ''
 
         # Check if package is multi-build and error out early if so
-        multi = osc.core.makeurl(self.apiurl, ['source', prj, pkg, '_multibuild'],
+        multi = osc.core.makeurl(self.API_URL, ['source', prj, pkg, '_multibuild'],
                                  query={'expand': 1})
+        is_multi_flavoured = False
         try:
             osc.core.http_GET(multi)
-            raise RuntimeError('Cannot handle multibuild package; bailing out.')
+            is_multi_flavoured = True # rpmspec won't work, so we have to grep manually
         except HTTPError as e:
             if (404 == e.getcode()):
-                # This means no _multibuild file found, so we are good to proceed
+                # This means no _multibuild file found, so we are good to proceed with rpmspec
                 pass
             else:
                 # Re-raise if something else has gone wrong
                 raise e
 
-        u = osc.core.makeurl(self.apiurl, ['source', prj, pkg, pkg + '.spec'],
+        u = osc.core.makeurl(self.API_URL, ['source', prj, pkg, pkg + '.spec'],
                              query={'expand': 1})
 
         try:
@@ -54,38 +60,44 @@ class SpecTags:
         if not path.exists(cachedir):
             mkdir(cachedir)
 
-        with NamedTemporaryFile(mode='w', suffix='.spec', dir=cachedir) as f:
-            f.write(spec)
-            f.flush()
+        if is_multi_flavoured:
+            self.url = self.RE_URL.search(spec).group().split(' ')[-1]
+            self.ver = self.RE_VER.search(spec).group().split(' ')[-1]
+            self.src_url = self.RE_SRC0.search(spec).group()
+        else:
+            with NamedTemporaryFile(mode='w', suffix='.spec', dir=cachedir) as f:
+                f.write(spec)
+                f.flush()
 
-            rpmspec_cmdline = '/usr/bin/rpmspec --srpm -q --qf "%{url} %{version}" '
-            rpmspec_cmdline += ' '.join([f'--define="{macro} %nil"' for macro in UNDEFINED_MACROS])
-            rpmspec_cmdline += f' {f.name}'
+                rpmspec_cmdline = '/usr/bin/rpmspec --srpm -q --qf "%{url} %{version}" '
+                rpmspec_cmdline += ' '.join([f'--define="{macro} %nil"' for macro in UNDEFINED_MACROS])
+                rpmspec_cmdline += f' {f.name}'
+                try:
+                    proc = run(rpmspec_cmdline, shell=True, capture_output=True, text=True, check=True)
+                    self.url, self.ver = proc.stdout.split()
+                except CalledProcessError as e:
+                    raise e
+
+                try:
+                    spec_parse  = run(['/usr/bin/rpmspec', '-P', f.name],
+                                      capture_output=True, text=True, check=True)
+                    spec_exp    = spec_parse.stdout
+                    self.src_url = self.RE_SRC0.search(spec_exp).group().split(' ')[-1]
+                except CalledProcessError:
+                    self.src_url = self.RE_SRC0.search(spec).group().split(' ')[-1]
+
             try:
-                proc = run(rpmspec_cmdline, shell=True, capture_output=True, text=True, check=True)
-                self.url, self.ver = proc.stdout.split()
-            except CalledProcessError as e:
-                raise e
+                # If srcURL is really a URL, then it will have at least 3 parts (http://...)
+                self.src_url.split('/')[2]
+            except IndexError:
+                # Get srcURL from _service file
+                service_file = osc.core.http_GET(u.replace(f"{pkg}.spec", "_service"))
+                service_xml  = b''.join(service_file.readlines())
+                service_root = etree.fromstring(service_xml.decode('utf-8'))
+                for f in service_root.findall(".//param[@name]"):
+                    if f.attrib["name"] == "url":
+                        self.src_url = f.text
 
-            try:
-                spec_parse  = run(['/usr/bin/rpmspec', '-P', f.name],
-                                  capture_output=True, text=True, check=True)
-                spec_exp    = spec_parse.stdout
-                self.src_url = self.re_src0.search(spec_exp).group().split(' ')[-1]
-            except CalledProcessError:
-                self.src_url = self.re_src0.search(spec).group().split(' ')[-1]
-
-        try:
-            # If srcURL is really a URL, then it will have at least 3 parts (http://...)
-            _ = self.src_url.split('/')[2]
-        except IndexError:
-            # Get srcURL from _service file
-            service_file = osc.core.http_GET(u.replace(f"{pkg}.spec", "_service"))
-            service_xml  = b''.join(service_file.readlines())
-            service_root = etree.fromstring(service_xml.decode('utf-8'))
-            for f in service_root.findall(".//param[@name]"):
-                if f.attrib["name"] == "url":
-                    self.src_url = f.text
 
     def Name(self):
         return self.name
